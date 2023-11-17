82fab1e1ca4c6e50a7f01013943f99d6d40d7670
Anna Sheehan <annasheehan@ssrb-vpn1-7-109.vpn.ufl.edu>
2023-11-17 21:14:47 +0000

    updated top-5 bar graph data

--- routers/emission-data.js
+++ routers/emission-data.js
@@ -4,6 +4,8 @@ const { emissionData, state, fuelType, energySector } = require("../lib/table");
 
 const emissionDataRouter = Router();
 
+// Query Sector Names and Codes
+// http://localhost:3000/emission-data/energy-sector
 emissionDataRouter.get("/energy-sector", async (req, res) => {
   try {
     const data = await query(`
@@ -19,6 +21,9 @@ emissionDataRouter.get("/energy-sector", async (req, res) => {
   }
 });
 
+
+// Query Data for line graph trends
+// localhost:3000/emission-data?stateCode=12&startYear=1970&endYear=2021
 emissionDataRouter.get("/", async (req, res) => {
   const stateCode = parseInt(req.query.stateCode);
   if (!stateCode || Number.isNaN(stateCode))
@@ -60,17 +65,49 @@ emissionDataRouter.get("/", async (req, res) => {
   }
 });
 
-// work on bar graph with top 5 energy sector queries - looks identical to previous
-// query just with new SQL statement to product data
-// find output at localhost:3000/emission-data/top-5-sectors
+// Query data for top-5-sector bar graph
+// localhost:3000/emission-data/top-5-sectors?stateCode=12&startYear=1970&endYear=2021
 emissionDataRouter.get("/top-5-sectors", async (req, res) => {
-  res.json({ message: "top-5-sectors" });
+  const stateCode = parseInt(req.query.stateCode);
+  if (!stateCode || Number.isNaN(stateCode))
+    return res.status(400).json({ error: "Invalid state code" });
+
+    const startYear = parseInt(req.query.startYear) || 1970;
+    const endYear = parseInt(req.query.endYear) || 2021;
+
+  try {
+    const data = await query(
+      
+      `
+      WITH avgEmission(year,sector_code,state_code,avg_emission) AS (
+        SELECT emis.year, emis.sector_code, emis.state_code, AVG(emis.emission) as avg_emission
+        FROM ${emissionData} emis
+        INNER JOIN ${state} s ON s.state_code = emis.state_code
+        INNER JOIN ${energySector} sect ON emis.sector_code = sect.sector_code
+        INNER JOIN ${fuelType} ft ON ft.fuel_type_code = emis.fuel_type_code
+        WHERE emis.emission IS NOT NULL
+        GROUP BY emis.year, emis.sector_code, emis.state_code
+        ORDER BY emis.year ASC, emis.state_code ASC
+      )
+      SELECT avgEmission.year, s.state_name, sect.sector_name, MAX(avgEmission.avg_emission) as max_avg_emission
+      FROM avgEmission
+      INNER JOIN ${state} s ON s.state_code=avgEmission.state_code
+      INNER JOIN ${energySector} sect ON sect.sector_code=avgEmission.sector_code
+      WHERE s.state_code = :stateCode 
+      AND avgEmission.year >= :startYear
+      AND avgEmission.year <= :endYear
+      GROUP BY avgEmission.year, s.state_name, sect.sector_name
+      ORDER BY avgEmission.year ASC, s.state_name ASC
+      `,
+    { stateCode, startYear, endYear }
+    );
+
+    res.json(data);
+  } catch (err) {
+    console.error("Failed to fetch emission top-5 bar graph data");
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch top-5 bar graph data" });
+  }
 });
 
 module.exports = emissionDataRouter;
-
-
-
-// HOW TO RUN
-// 1. in backend terminal, do 'npm run dev'
-// 2. in browser do, 'localhost:3000/emission-data?stateCode=12&startYear=1970&endYear=2021'
